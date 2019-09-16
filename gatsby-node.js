3f923894c4d3e2949e2dcd4b675c04891e004c19
Carlos Guimaraes <sekmet@open4g.com>
2019-09-16 16:47:50 -0300

    Initial commit!

---
+++ gatsby-node.js
@@ -0,0 +1,23 @@
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/node-apis/
+ */
+
+// You can delete this file if you're not using it
+
+const path = require('path')
+
+exports.onCreateWebpackConfig = ({ stage, actions, plugins }) => {
+
+    actions.setWebpackConfig({
+        resolve: {
+            alias: {
+                components: path.resolve(__dirname, 'src/components'),
+                //templates: path.resolve(__dirname, 'src/templates'),
+                //scss: path.resolve(__dirname, 'src/scss'),
+            },
+        },
+    })
+
+}
